d830f95405fcb6fefb3d20b5ac45e350b17d92a6
Rachel Castro <rcastro@myassetmap.com>
2025-03-29 00:02:40 -0600

    working

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ` `
 To run:
 uvicorn src.main:app --reload
+
+to send things to the server:
+curl -X POST http://127.0.0.1:8000/extract-text/
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from PIL import Image
 from groq import Groq
 import io 
 import os
+import base64
 # from groqing import groq_query
 from dotenv import load_dotenv
 load_dotenv() 
@@ -29,12 +30,13 @@ def read_root():
 async def extract_text():
 
     print("Pre processing with OCR")
-    await process_test("Hello")
+    await process_text("Hello")
     await access_groq()
+    await groq_image_access()
 
-async def process_test(input_text: str):
+async def process_text(input_text: str):
 
-    print("Calling something")
+    print("Calling something else")
 
 async def access_groq():
     client = Groq(
@@ -55,6 +57,47 @@ async def access_groq():
     print(chat_completion.choices[0].message.content)
 
     return chat_completion.choices[0].message.content
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+async def groq_image_access():
+   image_path = "./src/test-images/test-1.png"
+   base64_image = encode_image(image_path)
+   client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+   chat_completion = client.chat.completions.create(
+    model="llama-3.2-90b-vision-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Return the LaTeX of this expression."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                }
+            ],
+        }
+    ],
+    temperature=1,
+    max_completion_tokens=1024,
+    top_p=1,
+    stream=False,
+    stop=None,
+)
+   return chat_completion.choices[0].message.content
+    # print(chat_completion.choices[0].message.content)
+    # return(chat_completion.choices[0].message.content)
+
+
 
 if __name__ == "__main__":
     access_groq()   
